0b1ed8f4a9650e859e7c8b53ed5a70183a498e08
student <student@b7008312.localdomain>
2019-02-14 13:54:35 +0000

    added modules in bootsrap

--- enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
+++ enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
@@ -1,9 +1,68 @@
-package enrolsystem
+package com.enrol
 
 class BootStrap {
 
     def init = { servletContext ->
-    }
+def Computing = new Course(
+department: 'Computing',
+courseTitle: 'BSc Hon Computing',
+courseLeader:'Dr Michelle Murphy',
+courseCode: 'CS123',
+startDate: new Date('09/09/2019'),
+endDate: new Date('07/07/2023'),
+description : 'Lorem',
+numberOfStudents: 55,
+tuitionFees: 9000.60,
+studyMode: 'Full Time'
+).save()
+
+def Ben = new Student(
+studentName: 'Ben',
+studentID: '1',
+dob: new Date('06/08/1999'),
+isFundingAvailable: 'Yes',
+studentEmail: 'Ben@email.com',
+studentUsername: 'b7008312',
+studentPassword : 'red',
+course: 'BSc Hon Computing'
+).save()
+
+def Miles = new Student(
+studentName: 'Miles',
+studentID: '2',
+dob: new Date('16/10/1999'),
+isFundingAvailable: 'Yes',
+studentEmail: 'Miles@email.com',
+studentUsername: 'b9008312',
+studentPassword : 'pink',
+course: 'BSc Hon Computing'
+).save()
+
+def SysArchs = new Module(
+module_title: 'SysArchs',
+module_code: '1',
+credits: 30,
+lecturer: 'Tonderai',
+course: 'BSc Hon Computing',
+description: 'SysArchs'
+
+).save()
+
+def Tonderai = new Lecturer(
+fullName: 'Tonderai',
+post: 'Lecturer',
+subject: 'Computing',
+lecturerEmail: 'Tonderai@email.com',
+office: '9320',
+bio: 'SysArchs'
+
+).save()
+
+
+
+
+
+}
     def destroy = {
     }
 }
